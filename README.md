169ccee774d3c45a4b3a53af21ea177db175e21d
Alexis Leite <aleite@st.com.uy>
2024-07-26 14:18:24 -0300

    feat: first working label-insert version

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Allows to insert Apia labels on editor.
 
-**Add label**: Adds text getLabel('...').text
-**Add tooltip**: Adds text getLabel('...').tooltip
+- **Add label**: Adds text getLabel('...').text
+- **Add tooltip**: Adds text getLabel('...').tooltip
 
 Both commands add the necessary imports.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-async function runCommand(command: string,) {
+async function runCommand(command: string) {
 	const editor = vscode.window.activeTextEditor;
 	if (editor) {
 		const userInput = await vscode.window.showInputBox({
@@ -9,54 +9,57 @@ async function runCommand(command: string,) {
 
 
 		if (userInput !== undefined) {
-			// Add label
-			const selection = editor.selection;
-
 			// Add import statement
 			const document = editor.document;
 			const text = document.getText();
 
-			const editors: ((builder:vscode.TextEditorEdit) => void)[] = [];
+			const editors: ((builder: vscode.TextEditorEdit) => void)[] = [];
 
 			const generalMatch = text.match(/import \{([\w, ]+)} from '@apia\/util';/);
 			const importedMatch = text.match(/import \{(?:[\w, ]+)?getLabel(?:[\w, ]+)?\} from '@apia\/util';/);
 
-			if(!generalMatch && !importedMatch) {
-				editors.push(editBuilder => {
-					// Insert the import statement at the top of the file
+			if (!generalMatch && !importedMatch) {
+				editors.push(async (editBuilder) => {
 					const firstLine = document.lineAt(0);
-					editBuilder.insert(firstLine.range.start, `import { getLabel } from '@apia/util';\n`);
+					await editBuilder.insert(firstLine.range.start, `import { getLabel } from '@apia/util';\n`);
 				});
 			}
-			else if(generalMatch && !importedMatch) {
-				const entireRange = new vscode.Range(
-					document.positionAt(0),
-					document.positionAt(text.length)
-				);
-				const replacedText = text.replace(generalMatch[1], ` ${generalMatch[1].trim()}, getLabel `);
+			else if (generalMatch && !importedMatch) {
+				editors.push(async (editBuilder) => {
+					const document = editor.document;
+					const text = document.getText();
+					const entireRange = new vscode.Range(
+						document.positionAt(0),
+						document.positionAt(text.length)
+					);
+					const generalMatch = text.match(/import \{([\w, ]+)} from '@apia\/util';/);
+					const replacedText = text.replace(generalMatch![1], ` ${generalMatch![1].trim()}, getLabel `);
 
-				editors.push(editBuilder => {
-					editBuilder.replace(entireRange, replacedText);
+					await editBuilder.replace(entireRange, replacedText);
 				});
 
-			} 
+			}
 
-			editor.edit(editBuilder => {
-				editors.forEach(c => c(editBuilder));
-				editBuilder.replace(selection, `getLabel('${userInput}').${command}`);
+			await editor.edit(async (editBuilder) => {
+				const selection = editor.selection;
+				editBuilder.insert(selection.start, `getLabel('${userInput}').${command}`);
 			});
+
+			for await (const e of editors) {
+				await editor.edit(e);
+			}
 		}
 
-	} 
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push( vscode.commands.registerCommand('label-inserter.addLabel', async () => {
-    await runCommand('text');
-  }));
-	context.subscriptions.push( vscode.commands.registerCommand('label-inserter.addTooltip', async () => {
-    await runCommand('tooltip');
-  }));
+	context.subscriptions.push(vscode.commands.registerCommand('label-inserter.addLabel', async () => {
+		await runCommand('text');
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('label-inserter.addTooltip', async () => {
+		await runCommand('tooltip');
+	}));
 }
 
 export function deactivate() {
